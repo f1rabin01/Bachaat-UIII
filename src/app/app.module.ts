75a9ba2b96effdc5c6625de3de33fb6acf9e3cd6
coderabin01 <naga.rabin01@gmail.com>
2018-05-22 13:32:39 +0545

    Add input-field-text.component including spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { InputFieldEmailComponent } from './shared-utils/form-utils/input-field-email/input-field-email.component';
+import { InputFieldTextComponent} from './shared-utils/form-utils/input-field-text/input-field-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputFieldEmailComponent
+    InputFieldTextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared-utils/form-utils/input-field-text/input-field-text.component.spec.ts
+++ src/app/shared-utils/form-utils/input-field-text/input-field-text.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { User } from '../../../user';
+import {FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { InputFieldTextComponent } from './input-field-text.component';
 
@@ -8,18 +11,35 @@ describe('InputFieldTextComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputFieldTextComponent ]
+      declarations: [ InputFieldTextComponent ],
+      imports: [FormsModule, ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputFieldTextComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('input-field-text invalid when empty', () => {
+    let lastName = component.fcn;
+    let errors = lastName.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+  it('input-field-text validity', () => {
+    let lastName = component.fcn;
+    component.minLength = 3;
+    fixture.detectChanges();
+    // let minlength = component.fcn.minLength;
+    lastName.setValue('Ra');
+    let errors = lastName.errors || {};
+    expect(errors['required']).toBeFalsy();
+    // expect(errors['minlength']).toBeFalsy();
   });
 });
